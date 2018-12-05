72ab1c571b326f3a63c41debd8900dcaa98e85ed
Vladislav <vlad.raba@mail.ru>
2018-12-05 23:52:15 +0300

    First commit

---
+++ Roots.py
@@ -0,0 +1,24 @@
+
+The first version of a py file
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch master
+#
+# Initial commit
+#
+# Changes to be committed:
+#	new file:   QuadraricRoots.py
+#
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch master
+#
+# Initial commit
+#
+# Changes to be committed:
+#	new file:   QuadraricRoots.py
+#
